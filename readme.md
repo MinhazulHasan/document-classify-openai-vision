600dd927b0f650e0e0d97ff9a94f969de3db3bce
MinhazulHasan <minhazul.hasan.sohan@gmail.com>
2024-08-06 12:27:24 +0600

    add readme.md

---
+++ readme.md
@@ -0,0 +1,3 @@
+python -m venv doc_classify_venv
+.\doc_classify_venv\Scripts\activate
+pip install -r requirements.txt
--- tests/test_utils/test_helpers.py
+++
@@ -1,7 +0,0 @@
-from app.utils.helpers import encode_image
-
-def test_encode_image():
-    test_image = b"test image content"
-    encoded = encode_image(test_image)
-    assert isinstance(encoded, str)
-    assert encoded == "dGVzdCBpbWFnZSBjb250ZW50"
